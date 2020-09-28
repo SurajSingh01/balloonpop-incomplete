90561510794fa5099bdbc65c41dadb0cc336cfab
SurajSingh01 <singhsurajd03@gmail.com>
2020-09-28 16:56:50 +0530

    js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,92 +1,131 @@
-let colors = ['yellow', 'red', 'blue', 'violet','green'];
+let colors = ['yellow', 'red', 'blue', 'violet', 'green'];
 let windowWidth = window.innerWidth;
 let windowHeight = window.innerHeight;
-let heightOfBalloon = 200;
-let widthOfBalloon = 100;
+let body = document.body;
 let scores = document.querySelectorAll('.score');
-let numberOfBalloonPop = 0;
-let totalBalloonToBePoped = 10;
-let currentBalloon = 0;
-let gameOver =false;
+let num = 0;
+let total = 100;
+let currentBallon = 0;
+let gameOver = false;
 let totalShadow = document.querySelector('.total-shadow');
-let winner = document.querySelector('.win');
-let looser = document.querySelector('.lose');
+let startBtn = document.querySelector('.start-game-button');
 
 function createBalloon() {
 	let div = document.createElement('div');
-	let rand = Math.floor(Math.random() * colors.length);    //Random variable for color picking
-	div.className = 'balloon balloon-' + colors[rand];       //appended class ballon-red,balloon-green etc...
-	
-	rand = Math.floor(Math.random() * (windowWidth - 100));    //Added random number to obtain different position on screen
-	div.style.left = rand + 'px';							//Moving to left with specific pixel
-	div.dataset.number = currentBalloon; //dataset is the properyt to ssign number or object number is the name of variable therfore the name of the attribute become data-number
-	currentBalloon++;
-	document.body.appendChild(div);
+	let rand = Math.floor(Math.random()*colors.length);
+	div.className = 'balloon balloon-'+colors[rand];
+
+	rand = Math.floor(Math.random() * (windowWidth - 100));
+	div.style.left = rand + 'px';
+	div.dataset.number = currentBallon;
+	currentBallon++;
+
+	body.appendChild(div);
 	animateBalloon(div);
 }
-function animateBalloon(elem) {
-	let pos = 0; 											//position of the ballon
-	let interval = setInterval(frame, 10); 					//function frame will be caled after every 10 millisecond
- 
-	function frame() {
-		if(pos >=(windowHeight + 200)){						// condition for function will stop at top
+
+function animateBalloon(elem){
+	let pos = 0;
+	let random = Math.floor(Math.random() * 6 - 3);
+	let interval = setInterval(frame, 12 - Math.floor(num / 10) + random);
+
+	function frame(){
+		if(pos >= (windowHeight + 200) && (document.querySelector('[data-number="'+elem.dataset.number+'"]') !== null)) {
 			clearInterval(interval);
-			gameOver=true;
-			//deleteBalloon(elem);
-		}
-		else{
+			gameOver = true;
+		} else{
 			pos++;
 			elem.style.top = windowHeight - pos + 'px';
 		}
 	}
-
 }
 
 function deleteBalloon(elem){
-	if(document.querySelector('[data-number=" '+elem.dataset.number+' "]' !== null)) {    //This function will only work when there is some number in data-number
-	elem.remove();
-	numberOfBalloonPop++;
+		elem.remove();
+		num++;
+		updateScore();
+		playBallSound();
+}
+
+function playBallSound(){
+	let audio = document.createElement('audio');
+	audio.src = 'sounds/pop.mp3';
+	audio.play();
+}
+
+function updateScore(){
+	for(let i = 0; i < scores.length; i++){
+		scores[i].textContent = num;
+	}
+}
+
+function startGame(){
+	restartGame();
+	let timeout = 0;
+
+	let loop = setInterval(function(){
+		timeout = Math.floor(Math.random() * 600 - 100);
+		if(!gameOver && num !== total){
+			createBalloon();
+		} else if(num !== total) {
+			clearInterval(loop);
+			totalShadow.style.display = 'flex';
+			totalShadow.querySelector('.lose').style.display = 'block';
+		} else {
+			clearInterval(loop);
+			totalShadow.style.display = 'flex';
+			totalShadow.querySelector('.win').style.display = 'block';
+		}
+		
+	}, 800 + timeout);
+}
+
+function restartGame(){
+	let forRemoving = document.querySelectorAll('.balloon');
+	for(let i = 0; i < forRemoving.length; i++){
+		forRemoving[i].remove();
+	}
+	gameOver = false;
+	num = 0;
 	updateScore();
-	}
 }
 
-/*To ccalculate the score */
-function updateScore() {
-	for (let i=0; i< scores.length; i++){
-		scores[i].textContent = numberOfBalloonPop;
+document.addEventListener('click', function(event){
+	if(event.target.classList.contains('balloon')){
+		deleteBalloon(event.target);
 	}
-}
- /* To amimate the creation of ballon */
- function startGame(){
- let loop = setInterval(function(){
- 		if(!gameOver && numberOfBalloonPop !== totalBalloonToBePoped) {
- 			createBalloon();
- 		} else if(num !== totalBalloonToBePoped){
- 			clearInterval(loop);
- 			totalShadow.style.display = 'flex'; //in css we have done none 
- 			looser.style.display = 'block';
- 		}else{
- 			clearInterval(loop);
- 			totalShadow.style.display = 'flex';
- 			winner.style.display = 'block';
- 		}
- 	} , 800);
- }
+})
 
-/*let balloons =document.querySelectorAll('.balloon');						/*this code is not working because event listener will not work for prdefined object on webpage.
+document.querySelector('.restart').addEventListener('click', function(){
+	totalShadow.style.display = 'none';
+	totalShadow.querySelector('.win').style.display = 'none';
+	totalShadow.querySelector('.lose').style.display = 'none';
 
-for (let i=0; i< balloons.length; i++) {									to overcome this , using event delegation .
-	balloons[i].addEventListener('click',function(){
-		deleteBalloon(balloons[i])
-	})
-}*/ 
- /*Event deligation  for popup*/
- document.addEventListener('click',function(event){
- 	if(event.target.classList.contains('balloon')){
- 		deleteBalloon(event.target);
- 	}
- })
+	startGame();
+});
+
+document.querySelector('.cencel').addEventListener('click', function(){
+	totalShadow.style.display = 'none';
+});
+
+startBtn.addEventListener('click', function() {
+	startGame();
+	document.querySelector('.bg-music').play();
+	document.querySelector('.start-game-window').style.display = 'none';
+});
 
 
-/* To start th game */
-startGame()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
